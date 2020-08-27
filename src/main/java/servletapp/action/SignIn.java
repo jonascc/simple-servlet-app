5a54100f17657a73b3a1894b42f357804087f579
jonascc <jonasccarmo@gmail.com>
2020-08-27 17:33:53 -0300

    Implementing "Action" interface

--- src/main/java/servletapp/action/SignIn.java
+++ src/main/java/servletapp/action/SignIn.java
@@ -1,5 +1,13 @@
 package servletapp.action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class SignIn implements Action {
+
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
