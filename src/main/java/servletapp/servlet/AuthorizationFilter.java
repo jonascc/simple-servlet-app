dd4ccb7dfe2667642932521334bcd2ebdeafd6a2
jonascc <jonasccarmo@gmail.com>
2020-08-27 10:54:51 -0300

    First commit

---
+++ src/main/java/servletapp/servlet/AuthorizationFilter.java
@@ -0,0 +1,63 @@
+package servletapp.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import servletapp.model.User;
+
+/**
+ * Servlet Filter implementation class AuthorizationFilter
+ */
+public class AuthorizationFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public AuthorizationFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("Authorization filter.");
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+		HttpSession session = request.getSession();
+		boolean isSignedIn = session.getAttribute("signedInUser") != null;
+
+		
+
+		if (isSignedIn) {
+			request.setAttribute("isSignedIn", isSignedIn);
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
