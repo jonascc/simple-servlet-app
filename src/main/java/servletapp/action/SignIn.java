52c17ec8ac71da10b4f3ee189e232c201dcd93d1
jonascc <jonasccarmo@gmail.com>
2021-03-21 16:15:01 -0300

    Search for the user in the db

--- src/main/java/servletapp/action/SignIn.java
+++ src/main/java/servletapp/action/SignIn.java
@@ -1,13 +1,54 @@
 package servletapp.action;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import servletapp.model.User;
+import servletapp.persistence.ConnectionPool;
+import servletapp.persistence.dao.UsersDao;
+import servletapp.persistence.util.DataBaseInitializer;
 
 public class SignIn implements Action {
 
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		User user = new User.Builder().email(email).password(password).build();
+		
+		try {
+			try (Connection connection = new ConnectionPool().getConnection();
+					Statement statement = connection.createStatement()) {
+
+				DataBaseInitializer init = new DataBaseInitializer();
+				statement.executeUpdate(init.sqlScriptToString("createdb.sql"));
+
+				connection.setAutoCommit(false);
+				UsersDao dao = new UsersDao(connection);
+
+				User userSignedIn = dao.retrieveUser(user);
+				//connection.commit();
+				
+				if(userSignedIn != null) {
+					HttpSession session = request.getSession();
+					session.setAttribute("signedInUser", userSignedIn);
+					return "redirect:app?action=UserPage";
+				} else {
+					return "redirect:app?action=SignInForm";
+				}
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 
 }
