f2f2c768a6290cef0f8702e51aba4f029b6a7cc1
jonascc <jonasccarmo@gmail.com>
2021-03-21 16:02:01 -0300

    First commit
    Perform operations on the data base related to the users,
    like insert new user that has signed up.

---
+++ src/main/java/servletapp/persistence/dao/UsersDao.java
@@ -0,0 +1,65 @@
+package servletapp.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import servletapp.model.User;
+
+public class UsersDao {
+	
+	
+private final Connection con;
+	
+	public UsersDao(Connection con) {
+		this.con = con;
+	}
+	
+	public void insert(User user) throws SQLException {
+		String sql = "Insert into users "
+				+ "(name, lastName, email, password)"
+				+ "values(?, ?, ?, ?)";
+		
+		try (PreparedStatement stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			stmt.setString(1, user.getName());
+			stmt.setString(2, user.getLastName());
+			stmt.setString(3, user.getEmail());
+			stmt.setString(4, user.getPassword());
+			
+			stmt.execute();
+			try (ResultSet rs = stmt.getGeneratedKeys()) {
+				if (rs.next()) {
+					int id = rs.getInt("id");
+					user.setId(id);
+				}
+			}
+		}
+	}
+	
+	public User retrieveUser(User user) throws SQLException {
+		String sql = "select * from users where email = ? and password = ?";
+		
+		try (PreparedStatement stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			stmt.setString(1, user.getEmail());
+			stmt.setString(2, user.getPassword());
+			
+			stmt.execute();
+			try (ResultSet rs = stmt.getResultSet()) {
+				if (rs.next()) {
+					int id = rs.getInt("id");
+					String name = rs.getString("name");
+					String lastName = rs.getString("lastName");
+					String email = rs.getString("name");
+					return new User.Builder().id(id).name(name).lastName(lastName).email(email).build();
+				} else {
+					return null;
+				}
+			}
+		}
+		
+	}
+	
+
+}
