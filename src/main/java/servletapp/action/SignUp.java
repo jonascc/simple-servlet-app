9d82c5729e992017fd0d0709e02b8f2c9d291c11
jonascc <jonasccarmo@gmail.com>
2021-03-21 16:14:11 -0300

    Update the db with the new user

--- src/main/java/servletapp/action/SignUp.java
+++ src/main/java/servletapp/action/SignUp.java
@@ -1,13 +1,47 @@
 package servletapp.action;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import servletapp.model.User;
+import servletapp.persistence.ConnectionPool;
+import servletapp.persistence.dao.UsersDao;
+import servletapp.persistence.util.DataBaseInitializer;
 
 public class SignUp implements Action {
 
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		String name = request.getParameter("name");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		User user = new User(name, lastName, email, password);
+
+		try {
+			try (Connection connection = new ConnectionPool().getConnection();
+					Statement statement = connection.createStatement()) {
+
+				DataBaseInitializer init = new DataBaseInitializer();
+				statement.executeUpdate(init.sqlScriptToString("createdb.sql"));
+
+				connection.setAutoCommit(false);
+				UsersDao dao = new UsersDao(connection);
+
+				dao.insert(user);
+				connection.commit();
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return "forward:signUpSuccess.jsp";
 	}
 
 }
